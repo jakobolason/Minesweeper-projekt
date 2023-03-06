52b68a5a171d89d0c49fcad499166e0d42395b02
jakobolason <jakobolason@gmail.com>
2023-03-06 11:22:27 +0100

    Working inputs and click

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -1,35 +1,84 @@
 
-game_field = {1: [1, 0, 0, 0, 0, 0, 0, 0, 0], 2: [0, 1, 0, 0, 0, 0, 0, 0, 0], 3: [0, 0, 0, 0, 0, 0, 0, 0, 0], \
+bomb_field = {1: [0, 0, 0, 0, 0, 0, 0, 0, 0], 2: [0, 0, 0, 0, 0, 0, 0, 0, 0], 3: [0, 1, 0, 0, 0, 0, 0, 0, 0], \
               4: [0, 0, 0, 0, 0, 0, 0, 0, 0], 5: [0, 0, 0, 0, 0, 0, 0, 0, 0], 6: [0, 0, 0, 0, 0, 0, 0, 0, 0], \
                 7: [0, 0, 0, 0, 0, 0, 0, 0, 0], 8: [0, 0, 0, 0, 0, 0, 0, 0, 0], 9: [0, 0, 0, 0, 0, 0, 0, 0, 0]}
-# for key in game_field.keys():
-#     while True:
-#         for number in game_field[key]:
-#             print("(X)".format(key=key, number=number), end=" ")
-#         print("\n")
-#         break
 
-def check_for_bombs(row, column):
-    cell = game_field[row][column]
-    print(cell)
+visual_field = {1: [0, 0, 0, 0, 0, 0, 0, 0, 0], 2: [0, 0, 0, 0, 0, 0, 0, 0, 0], 3: [0, 0, 0, 0, 0, 0, 0, 0, 0], \
+              4: [0, 0, 0, 0, 0, 0, 0, 0, 0], 5: [0, 0, 0, 0, 0, 0, 0, 0, 0], 6: [0, 0, 0, 0, 0, 0, 0, 0, 0], \
+                7: [0, 0, 0, 0, 0, 0, 0, 0, 0], 8: [0, 0, 0, 0, 0, 0, 0, 0, 0], 9: [0, 0, 0, 0, 0, 0, 0, 0, 0]}
+
+def click_cell(row, column):
+    cell = bomb_field[row][column]
     if cell == 1:
         print("You clicked on a bomb, you busted!")
+        return busted
     else:
         print("You're okay")
+    visual_field[row][column] = 1
 
+
+def bombs_nearby(row, column):
     #checks row - 1: column -1, column, column +1
     #       row: column -1, column +1
     #       row +1: column -1, column, column +1    
-    adjacent_cells = game_field[row-1][column-1:column+2]
-    adjacent_cells.extend(game_field[row][column-1:column+2])
-    adjacent_cells.extend(game_field[row+1][column-1:column+2])
+    adjacent_cells = []
+    try:
+        adjacent_cells = bomb_field[row-1][column-1:column+2]
+    except KeyError:
+        pass
+    try:
+        adjacent_cells.extend(bomb_field[row][column-1:column+2])
+    except KeyError:
+        pass
+    try:
+        adjacent_cells.extend(bomb_field[row+1][column-1:column+2])
+    except KeyError:
+        pass
     bombs_nearby = 0
     for cell in adjacent_cells:
         if cell == 1:
             bombs_nearby += 1
     return bombs_nearby
 
-check_for_bombs(3, 2)
+# show visual field
+def show_field():
+    for key in visual_field.keys():
+        while True:
+            for number in visual_field[key]:
+                if number == 0:
+                    print("(X)".format(key=key, number=number), end=" ")
+                elif number == 1:
+                    print(" {bombs} ".format(bombs=bombs_nearby(key, number)), end=" ")
+                else:
+                    continue
+            print("\n")
+            break
+
+class Playing():
+    def play(self):
+        busted = False
+        while not busted:
+            print("*************************")
+            show_field()
+            print("*************************")
+            user_row = int(input("which row would you like to click? "))
+            while user_row > 8:
+                user_row = int(input("You can pick a number between 0 and 8 "))
+            user_column = int(input("which column? "))
+            while user_column > 8:
+                user_column = int(input("You can pick a number between 0 and 8 "))
+            click_cell(user_row, user_column)
+
+            cell = bomb_field[user_row][user_column]
+            if cell == 1:
+                print("You clicked on a bomb, you busted!")
+                return busted
+            else:
+                continue
+                
+
+game_one = Playing()
+game_one.play()
 
 
 
