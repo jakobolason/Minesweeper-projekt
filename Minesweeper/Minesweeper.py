ee4c9762e200d726de24cd43e57cea0a5665f7a7
jakobolason <jakobolason@gmail.com>
2023-03-07 14:23:08 +0100

    improved UI and tinkered with flood_fill()

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -11,7 +11,6 @@ def shuffle_bombs_in(amount):
     for bombs in range(amount):
         random_key = random.randint(1, 9)
         random_cell = random.randint(0,8)
-        print(random_key, random_cell)
         bomb_field[random_key][random_cell-1] = 1
 
 def click_cell(row, column, player=True):
@@ -40,40 +39,48 @@ def bombs_nearby(row, column):
     return bombs_nearby
 
 def flood_fill(row, column):
-    if bombs_nearby(row, column) != 0:
-        return
-    adjacent_cells = []
-    for new_row in range(max(1, row-1), min(row+2, len(bomb_field)+1)):
-        for new_column in range(max(1, column-1), min(column+2, len(bomb_field)+1)):
-            if bomb_field[new_row][new_column] == 0 and visual_field[new_row][new_column] == 0:
-                adjacent_cells.append((new_row, new_column))
-    
-    for new_row, new_column in adjacent_cells:
-        print(new_row, new_column)
-        visual_field[new_row][new_column] = 1
-
+    print(bombs_nearby(row, column))
+    if bombs_nearby(row, column) == 0:
+        adjacent_cells = []
+        for new_row in range(max(1, row-1), min(row+2, len(bomb_field)+1)):
+            for new_column in range(max(1, column-1), min(column+2, len(bomb_field)+1)):
+                if bomb_field[new_row][new_column] == 0 and visual_field[new_row][new_column] == 0:
+                    adjacent_cells.append([new_row, new_column])
+        print(adjacent_cells)
+        for new_row, new_column in adjacent_cells:
+            print(new_row, new_column)
+            visual_field[new_row][new_column] = 1
 
 # show visual field
+# column bliver printet før de andre
+# rows bliver printet som indexet af row. 
 def show_field():
-    for key in visual_field.keys():
+    print("\n\t\t      MINESWEEPER")
+    print("-"*57)
+    print("", end="    ")
+    for column in range(9):
+        print("{column}".format(column=column), end="   | ")
+    print()
+    print("_"*57)
+    for index, key in enumerate(visual_field.keys()):
+        print(index, end=" | ")
         while True:
             for number in visual_field[key]:
                 if number == 0:
-                    print("(X)".format(key=key, number=number), end=" ")
+                    print("(X)".format(key=key, number=number), end=" | ")
                 elif number == 1:
-                    print(" {bombs} ".format(bombs=bombs_nearby(key, number)), end=" ")
+                    print(" {bombs} ".format(bombs=bombs_nearby(key, number)), end=" | ")
                 else:
                     continue
             print("\n")
             break
+    print("_"*57)
 
 class Playing():
     def play(self):
         busted = False
         while not busted:
-            print("*"*40)
             show_field()
-            print("*"*40)
             user_row = int(input("which row would you like to click? "))
             while user_row > 8 and user_row < 0 or not ValueError:
                 user_row = int(input("You can pick a number between 0 and 8 "))
@@ -93,9 +100,6 @@ class Playing():
 shuffle_bombs_in(10)
 game_one = Playing()
 game_one.play()
-
-
-
 
 # rules: if the adjacent 8 cells do not include a bomb, show the adjacent cells \
 #  if there are adjacent cells with bombs, show the amount of bombs in the area
